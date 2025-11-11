ac88cb44beb141dd0669d7fbd59a3a1c14d00ce8
gabrielandrade131 <gabrielandrade131@gmail.com>
2025-11-11 01:03:33 -0300

    feat: create car and price models

--- FuelFit/models.py
+++ FuelFit/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=30)
+    year = models.IntegerField()
+    gasoline_consumption = models.IntegerField()
+    ethanol_consumption = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.model} {self.year}"
+
+
+class Price(models.Model):
+    gasoline_price = models.DecimalField(max_digits=6, decimal_places=3)
+    ethanol_price = models.DecimalField(max_digits=6, decimal_places=3)
